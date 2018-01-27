87eaee822b5099d1a46241e22995ab3d596d3144
eth-button <eth-button@users.noreply.github.com>
2018-01-27 03:39:49 -0800

    Initial commit
    It works!

---
+++ index.js
@@ -0,0 +1,43 @@
+import Blockie from 'lib-blockies';
+import {
+  isValidPrivate,
+  privateToPublic,
+  publicToAddress,
+  bufferToHex
+} from 'ethereumjs-util';
+import { randomBytes } from 'crypto';
+
+export function generatePrivateKey() {
+  let privateKey
+  do {
+    privateKey = randomBytes(32)
+  } while (!isValidPrivate(privateKey));
+
+  return privateKey
+}
+
+export function generateAddress() {
+  const privateKey = generatePrivateKey();
+  const publicKey = privateToPublic(privateKey);
+  const address = bufferToHex(publicToAddress(publicKey));
+  const blockie = new Blockie(address);
+
+  return {
+    privateKey: bufferToHex(privateKey),
+    publicKey: bufferToHex(publicKey),
+    address,
+    blockie
+  };
+}
+
+export function makeBlocks(quantity) {
+  const blocks = [];
+  for(let i = 0; i < quantity; i++) {
+    blocks.push(generateAddress());
+  }
+  return blocks;
+}
+
+export function generateWallet(privateKey) {
+  console.log(privateKey);
+}
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+var fs = require('fs');
+
+var package = fs.readFileSync('package.json', 'utf-8');
+package = JSON.parse(package);
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    library: "blockiePicker",
+    filename: "dist/" + package.name + ".js"
+  },
+  plugins: [new UglifyJSPlugin()]
+};
